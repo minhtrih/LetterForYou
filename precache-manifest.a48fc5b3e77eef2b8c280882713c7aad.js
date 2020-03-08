ffd357db4c9316b451b33a891a02b57a138b1a66
Hoang Minh Tri <tri.hm@agiletech.vn>
2020-03-08 09:53:16 +0100

    Updates

---
+++ precache-manifest.a48fc5b3e77eef2b8c280882713c7aad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08987d85edac6c0dac750f0ffa3d290",
+    "url": "/LetterForYou/index.html"
+  },
+  {
+    "revision": "bd77f3ec25f60c462e85",
+    "url": "/LetterForYou/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "644992cf89e1fb95a8d6",
+    "url": "/LetterForYou/static/js/2.f8384d6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/LetterForYou/static/js/2.f8384d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd77f3ec25f60c462e85",
+    "url": "/LetterForYou/static/js/main.c9ee6422.chunk.js"
+  },
+  {
+    "revision": "e9c401437e648643ec0b",
+    "url": "/LetterForYou/static/js/runtime-main.ae810f8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LetterForYou/static/media/logo.5d5d9eef.svg"
+  }
+]);
